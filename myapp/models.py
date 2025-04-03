66818f56280d8e7b8b3ab82c9855ec6b59b9a436
IyanuKwent <olandria.iankent@gmail.com>
2025-04-04 03:12:28 +0800

    changes in models

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class Task(models.Model):
-    task = models.CharField(max_length=255)  # Task description
-    completed = models.BooleanField(default=False)  # Task status
+    text = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.task
+        return self.text
