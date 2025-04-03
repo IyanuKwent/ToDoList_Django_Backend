599008a247619d12a25ed669fa7181781bd95dc4
IyanuKwent <olandria.iankent@gmail.com>
2025-04-04 02:18:41 +0800

    remove date and time

--- myapp/views.py
+++ myapp/views.py
@@ -11,5 +11,5 @@ def home_view(request):
 
 class TaskViewSet(viewsets.ModelViewSet):
 
-    queryset = Task.objects.all().order_by('-created_at')  # Fetch all tasks
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
