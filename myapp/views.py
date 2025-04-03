ad3a7de44ad23b36734ad0292973e478ca84fe0a
IyanuKwent <olandria.iankent@gmail.com>
2025-04-04 02:45:45 +0800

    added back urls and views adding add function

--- myapp/views.py
+++ myapp/views.py
@@ -4,10 +4,20 @@ from django.http import JsonResponse  # Import JsonResponse for the home view
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.decorators import api_view
 
 def home_view(request):
     """Return a welcome message."""
     return JsonResponse({'message': 'Welcome to the API!'})  # Simple welcome message
+
+@api_view(['POST'])
+def add_task(request):
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Save the new task to the database
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 class TaskViewSet(viewsets.ModelViewSet):
 
