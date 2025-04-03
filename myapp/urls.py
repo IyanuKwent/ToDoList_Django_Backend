74fc8e05d7e220f9f2ae0305740ccd81dda6284a
IyanuKwent <olandria.iankent@gmail.com>
2025-04-04 00:07:02 +0800

    please work

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_tasks, add_task, delete_task, update_task
+from . import views
+
 
 urlpatterns = [
-    path('tasks/', get_tasks, name='get_tasks'),
-    path('tasks/add/', add_task, name='add_task'),
-    path('tasks/delete/<int:pk>/', delete_task, name='delete_task'),
-    path('tasks/update/<int:pk>/', update_task, name='update_task'),
+    path('tasks/', views.get_tasks, name='get_tasks'),
+    path('tasks/add/', views.add_task, name='add_task'),
+    path('tasks/<int:pk>/delete/', views.delete_task, name='delete_task'),
+    path('tasks/<int:pk>/update/', views.update_task, name='update_task'),
 ]
